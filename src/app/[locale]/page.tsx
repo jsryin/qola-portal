92e83d1575d58a545bccbe2faf2fdca6dd72f49f
jsryin <1905273480@qq.com>
2026-01-16 11:36:26 +0800

    feat: 门户页面，路径修改为 /国家编码/语言编码

--- src/app/[locale]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { use } from "react";
-import ClientPortal from "./ClientPortal";
-
-export default function Page({ params }: { params: Promise<{ locale: string }> }) {
-  // Await the params
-  const { locale } = use(params);
-
-  return <ClientPortal locale={locale} />;
-}
